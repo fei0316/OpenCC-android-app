116a9055ba984c16ced5a42e41166b370fe37f9a
Fei <fei_fei_0316@hotmail.com>
2020-04-25 15:01:37 -0700

    First Commit.

---
+++ app/src/main/java/com/example/tsconverter/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.tsconverter;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.zqc.opencc.android.lib.ChineseConverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import static com.zqc.opencc.android.lib.ConversionType.HK2S;
+import static com.zqc.opencc.android.lib.ConversionType.TW2SP;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        final Button conv_button = (Button) findViewById(R.id.convert_button);
+        conv_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText conv_text = (EditText) findViewById(R.id.editText_convert);
+                String text = conv_text.getText().toString();
+                String converted = openCCConv(text, 1);
+
+                Toast toast = Toast.makeText(getApplicationContext(),text, Toast.LENGTH_LONG);
+                toast.show();
+                conv_text.setText(converted);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public String openCCConv (String from, int type) {
+
+        String result = ChineseConverter.convert(from, HK2S, getApplicationContext());
+        return result;
+    }
+
+
+}
